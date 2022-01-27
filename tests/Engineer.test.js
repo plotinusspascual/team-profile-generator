a937da3217302e0c27319955ce8342e172fffbea
plotinusspascual <plotinusspascual@gmail.com>
2022-01-27 15:32:51 -0800

    added and passed successfully engineer.test

---
+++ tests/Engineer.test.js
@@ -0,0 +1,24 @@
+const Engineer = require("../lib/Engineer");
+
+describe("Engineer", () => {
+  it("Should check if it can store github", ()=>{
+    const github = "plotinusspascual";
+    const engineer = new Engineer("Jose", 2, "plotinusspascual@gmail.com", github);
+    expect(engineer.github).toBe(github);
+  });
+
+  describe("getGithub", () => {
+    it("Should return Manager instead of Employee", () => {
+      const github = "plotinusspascual";
+      const engineer = new Engineer("Jose", 2, "plotinusspascual@gmail.com", github);
+      expect(engineer.getGithub()).toBe(github);
+    });
+  });
+  describe("getRole", () => {
+    it("Should return Engineer instead of Employee", () => {
+        const role = "Engineer";
+        const engineer = new Engineer("Jose", 2, "plotinusspascual@gmail.com", "plotinusspascual");
+        expect(engineer.getRole()).toBe(role);
+    });
+  });
+});
